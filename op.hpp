46482edecf78f4ca1ba160b4c8fd00701e1f1867
libii <libisin@gmail.com>
2021-01-24 21:56:07 -0800

    Added CMakeList.txt, Main and Op Class

--- op.hpp
+++ op.hpp
@@ -2,12 +2,31 @@
 #define __OP_HPP__
 
 #include "base.hpp"
+#include <string>
 
 class Op : public Base {
+    private:
+        double myValue=0;
+        Base* leftChild=NULL;
+        Base* rightChild=NULL;
+
     public:
-        Op(double value) : Base() { }
-        virtual double evaluate() { return 0.0; }
-        virtual std::string stringify() { return ""; }
+        Op(double value) : Base() { 
+            myValue = value;
+        }
+        virtual double evaluate() { return myValue; }
+        virtual std::string stringify() { 
+            return std::to_string(myValue); }
+        
+        double getValue(){
+            return myValue;
+        }
+
+        void setValue(double v){
+            myValue=v;
+        }
+
+
 };
 
 #endif //__OP_HPP__
