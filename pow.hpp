f0d4ea3dca89ad2d9ea3d917138639e4b811b35e
UrbanCoffee <rudy.m.7854@gmail.com>
2021-01-26 14:05:37 -0800

    Added checks and more test for Pow class.

--- pow.hpp
+++ pow.hpp
@@ -2,12 +2,17 @@
 #define __POW_HPP__
 
 #include "base.hpp"
+#include <stdexcept>
+#include <limits>
+#include <cmath>
 
 class Pow : public Base {
     public:
         Pow(Base* left, Base* right) : Base() {
             //configure below to throw an error when right is negative
-            //if(right->evaluate() < 0){throw;}
+            if(left==nullptr || right == nullptr){throw std::invalid_argument("Null argument passed into Pow class.");}
+            //checks is left is negative and is right is a negative fraction. if true, returns error as this is causes an imaginary number.
+            if(left->evaluate() < 0 && !( right->evaluate() == floor(right->evaluate()) ) ){throw std::invalid_argument("Undefined. Imaginary number.");}
             lChild = left;
             rChild = right;
         }
